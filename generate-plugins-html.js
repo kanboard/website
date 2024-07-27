64b013091c1aa349fb182f0af6fec7606640ed22
e-labinnovations <mohammedashad23@gmail.com>
2024-07-27 13:33:16 +0530

    Tooltip content change and open readme on new tab

--- generate-plugins-html.js
+++ generate-plugins-html.js
@@ -7,84 +7,85 @@ const pluginsData = JSON.parse(fs.readFileSync("plugins.json", "utf-8"));
 
 // HTML template
 const template = `
-<div class="plugin-info">
-  <dt>
-    <a href="{{homepage}}">{{title}}</a>
-  </dt>
-  <dd>
-      {{{description}}}
-    <p>
-      <em>
-        <svg class="icon">
-          <use xlink:href="#people-fill"></use>
-        </svg>
-        <b>By {{author}}</b>
-      </em>
-      <em>Version: {{version}}</em>
-        • 
-      <em>{{last_updated}}</em>
-    </p>
-    <p>
-      <img
-        alt="License: {{license}}"
-        data-tippy-content="License: {{license}}"
-        src="https://badgen.net/static/license/{{license}}/blue"
-      />
-      <img
-        alt="Compatible Version: {{compatible_version}}"
-        data-tippy-content="Compatible Version: {{compatible_version}}"
-        src="https://badgen.net/static/KB/{{compatible_version}}/orange"
-      />
-    </p>
-    <p>
-      <a
-        href="{{readme}}"
-        >Readme</a
-      >
-    </p>
-    <div class="structure-list">
-      {{#has_schema}}
-      <svg class="icon orange" data-tippy-content="This plugin using schema">
-        <use xlink:href="#database-add-icon"></use>
-      </svg>
-      {{/has_schema}}
-      {{^has_schema}}
-      <svg
-        class="icon green"
-        data-tippy-content="This plugin not using any schema"
-      >
-        <use xlink:href="#database-check-icon"></use>
-      </svg>
-      {{/has_schema}}
-      {{#has_hooks}}
-      <svg class="icon orange" data-tippy-content="This plugin uses Kanboard Hooks">
-        <use xlink:href="#hooks-add-icon"></use>
-      </svg>
-      {{/has_hooks}}
-      {{^has_hooks}}
-      <svg
-        class="icon green"
-        data-tippy-content="This plugin not using any Kanboard Hooks"
-      >
-        <use xlink:href="#hooks-check-icon"></use>
-      </svg>
-      {{/has_hooks}}
-      {{#has_overrides}}
-      <svg class="icon orange" data-tippy-content="This plugin override Kanboard data">
-        <use xlink:href="#overrides-add-icon"></use>
-      </svg>
-      {{/has_overrides}}
-      {{^has_overrides}}
-      <svg
-        class="icon green"
-        data-tippy-content="This plugin not override any Kanboard data"
-      >
-        <use xlink:href="#overrides-check-icon"></use>
-      </svg>
-      {{/has_overrides}}
-    </div>
-  </dd>
-</div>
+        <div class="plugin-info">
+          <dt>
+            <a href="{{homepage}}">{{title}}</a>
+          </dt>
+          <dd>
+              {{{description}}}
+            <p>
+              <em>
+                <svg class="icon">
+                  <use xlink:href="#people-fill"></use>
+                </svg>
+                <b>By {{author}}</b>
+              </em>
+              <em>Version: {{version}}</em>
+                • 
+              <em>{{last_updated}}</em>
+            </p>
+            <p>
+              <img
+                alt="License: {{license}}"
+                data-tippy-content="License: {{license}}"
+                src="https://badgen.net/static/license/{{license}}/blue"
+              />
+              <img
+                alt="Compatible Version: {{compatible_version}}"
+                data-tippy-content="Compatible Version: {{compatible_version}}"
+                src="https://badgen.net/static/KB/{{compatible_version}}/orange"
+              />
+            </p>
+            <p>
+              <a
+                href="{{readme}}"
+                target="_blank"
+                >Readme</a
+              >
+            </p>
+            <div class="structure-list">
+              {{#has_schema}}
+              <svg class="icon orange" data-tippy-content="This plugin includes database changes.">
+                <use xlink:href="#database-add-icon"></use>
+              </svg>
+              {{/has_schema}}
+              {{^has_schema}}
+              <svg
+                class="icon green"
+                data-tippy-content="This plugin does not contain any database changes."
+              >
+                <use xlink:href="#database-check-icon"></use>
+              </svg>
+              {{/has_schema}}
+              {{#has_hooks}}
+              <svg class="icon orange" data-tippy-content="The plugin incorporates Kanboard hooks.">
+                <use xlink:href="#hooks-add-icon"></use>
+              </svg>
+              {{/has_hooks}}
+              {{^has_hooks}}
+              <svg
+                class="icon green"
+                data-tippy-content="The plugin does not incorporate any Kanboard hooks."
+              >
+                <use xlink:href="#hooks-check-icon"></use>
+              </svg>
+              {{/has_hooks}}
+              {{#has_overrides}}
+              <svg class="icon orange" data-tippy-content="This plugin overrides existing templates.">
+                <use xlink:href="#overrides-add-icon"></use>
+              </svg>
+              {{/has_overrides}}
+              {{^has_overrides}}
+              <svg
+                class="icon green"
+                data-tippy-content="This plugin does not contain any template overrides."
+              >
+                <use xlink:href="#overrides-check-icon"></use>
+              </svg>
+              {{/has_overrides}}
+            </div>
+          </dd>
+        </div>
 `;
 
 // Generate HTML for each plugin
